d935fcdc036366bdce09f81fdd267239ad208745
viniciusAbrantes <vinicius_abrantes@live.com>
2018-05-29 17:39:39 -0300

    Agr vai

--- geraMatrizAleatoria/thread.c
+++ geraMatrizAleatoria/thread.c
@@ -2,7 +2,18 @@
 #include <locale.h>
 
 int main() {
-  printf("Teste\n");
-  printf("\e[H\e[2J");
+  //printf("\e[H\e[2J"); //Limpa o terminal
+  int num_linhas, num_colunas, num_threads;
+  double valor;
+  char nome_arq[20];
+  printf("Informe o nome do arquivo: ");
+  gets(nome_arq); //entre com o nome da matriz: "matriz.txt"
+  puts(nome_arq);
+  FILE *pfile = fopen(nome_arq, "r");
+  if(pfile != NULL) printf("Abriu certo\n");
+  else printf("Abriu errado\n");
+  //fscanf(pfile, "%lf", &valor);
+  //printf("Primeiro num = %f\n",valor);
+
   return 0;
 }

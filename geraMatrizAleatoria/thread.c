201986149c7899872b40290a639510e722508862
viniciusAbrantes <vinicius_abrantes@live.com>
2018-05-30 12:45:36 -0300

    Estou triste

--- geraMatrizAleatoria/thread.c
+++ geraMatrizAleatoria/thread.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <locale.h>
 
-typedef struct elemento_matriz {
-  double valor;
-  int x, y;
-  struct elemento_matriz *prox_l, *l_ant, *prox_c;
-}tmatriz;
-
-void insereElemento(tmatriz **ini_l, tmatriz **percorre, int i, int j, double valor);
-
 int main() {
-  //inicio da matriz esparça:
-  tmatriz *inicio = NULL;
-  tmatriz *ini_l = NULL, *percorre = NULL;
 
   //código para imprimir caracteres com acento:
   setlocale(LC_ALL, "portuguese");
@@ -37,57 +25,21 @@ int main() {
   printf("Informe o número de threads: ");
   scanf("%d", &num_threads);
 
-  //criando a primeirra linha da matriz:
-  fscanf(pfile, "%lf", &vtemp);
-  insereElemento(&ini_l, &percorre, 0, 0, vtemp);
-  inicio = ini_l;
-
-  //criando as demais linhas da matriz:
-  for(j = 1; j < num_colunas; j++) {
-    fscanf(pfile, "%lf", &vtemp);
-    insereElemento(&ini_l, &percorre, 0, j, vtemp);
+  //Criando a matriz
+  double matriz[num_linhas][num_colunas];
+  for(i = 0; i < num_linhas; i++) {
+    for(j = 0; j < num_colunas; j++) {
+      fscanf(pfile, "%lf", &vtemp);
+      matriz[i][j] = vtemp;
+    }
   }
 
-  for(i = 1; i < num_linhas; i++) {
+  //Printando os elementos para testar
+  for(i = 0; i < num_linhas; i++) {
     for(j = 0; j < num_colunas; j++) {
-      fscanf(pfile, "%lf", &valor);
-      insereElemento(&ini_l, &percorre, i, j, vtemp);
+      printf("(%d,%d) = %lf\n", i, j, matriz[i][j]);
     }
   }
 
   return 0;
-}
-
-void insereElemento(tmatriz **pini_l, tmatriz **ppercorre, int i, int j, double valor) {
-  tmatriz *novo_elemento = (tmatriz*) malloc(sizeof(tmatriz));
-
-  novo_elemento->valor = valor;
-  novo_elemento->x = i;
-  novo_elemento->y = j;
-  novo_elemento->prox_l = NULL;
-  novo_elemento->prox_c = NULL;
-  novo_elemento->l_ant = NULL;
-
-  //Para o primeiro elemento da matriz:
-  if(*ppercorre == NULL) {
-    *ppercorre = novo_elemento;
-    *pini_l = novo_elemento;
-  }
-
-  //Para os demais elementos da matriz:
-  else {
-    if(j == 0) { //Se for o primeiro elemento da linha
-      (*ppercorre)->prox_l = novo_elemento;
-      (*pini_l)->prox_l = novo_elemento;
-      novo_elemento->l_ant = *pini_l;
-      *ppercorre = novo_elemento;
-      *pini_l = novo_elemento;
-    }
-    else{ //Se não for o primeiro elemento da linha
-    (*ppercorre)->prox_c = novo_elemento;
-    novo_elemento->l_ant = (*pini_l)->l_ant;
-    *ppercorre = novo_elemento;
-    }
-  }
-  printf("Elemento (%d, %d) = %lf\n", novo_elemento->x, novo_elemento->y, novo_elemento->valor);
 }

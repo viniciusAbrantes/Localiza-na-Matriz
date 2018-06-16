955afa51006862f91018e6f7527b10b339a113fc
AlfredoFilho <alfredoalbelisb@gmail.com>
2018-06-16 16:59:38 -0300

    Comentarios

--- procuraValor/funções.c
+++ procuraValor/funções.c
@@ -1,6 +1,21 @@
+/**
+ * Conjunto de funcoes para carregar matriz de um arquivo, procurar determinado valore na matriz e imprimir cordenadas do número encontrado.
+ *
+ * Autor Alfredo Albélis, Cléofas Santos e Vinícius Pereira
+ *
+ * Ultima atualizacao: Sáb 16 Julho 2018 19:50:44
+ *
+ */
 #include <stdio.h>
 #include "funções.h" //Arquivo contendo o cabeçalho das funções
-
+/**
+ * Funcao para carregar matriz de um arquivo.
+ *
+ * @param *pfile Ponteiro para arquivo contendo os números que serão carregados.
+ * @param numa_linhas Numero de linhas da matriz.
+ * @param num_colunas Numero de colunas da matriz.
+ * @param matriz Nome da variável que receberá o arquivo.
+ */
 void geraMatriz(FILE *pfile, int num_linhas, int num_colunas, double matriz[num_linhas][num_colunas]) {
   double vtemp;
   int i, j;
@@ -14,7 +29,17 @@ void geraMatriz(FILE *pfile, int num_linhas, int num_colunas, double matriz[num_
     }
   }
 }
-
+/**
+ * Funcao para procurar determinado valore na matriz.
+ *
+ * @param arg contendo as seguintes variáveis:
+ *		  	num_linhas = num_linhas;
+ *   	    num_colunas = num_colunas;
+ *   	    num_thread = i;
+ *   	    qtd_threads = qtd_threads;
+ *   	    valor = valor;
+ *   	    matriz = matriz[0];.
+ */
 void* procuraValor(void* arg) {
   tipo_arg* parg = arg;
   int i, j;
@@ -27,7 +52,13 @@ void* procuraValor(void* arg) {
     }
   }
 }
-
+/**
+ * Funcao para imprimir cordenadas do número encontrado.
+ *
+ * @param numa_linhas Numero de linhas da matriz.
+ * @param num_colunas Numero de colunas da matriz.
+ * @param matriz Matriz binária.
+ */
 void printCoordenadas(int num_linhas, int num_colunas, double matriz[num_linhas][num_colunas]) {
   int i, j;
 
